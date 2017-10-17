05cb3fb49dcfc455c475d4140a52945076805ced
ewakors <ewakorszaczuk@vp.pl>
2017-10-17 21:41:31 +0200

    create color picker, change background color

--- grafikaPS2/Form1.Designer.cs
+++ grafikaPS2/Form1.Designer.cs
@@ -33,10 +33,24 @@
             this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.saveAsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.tabControl = new System.Windows.Forms.TabControl();
             this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
             this.saveFileDialog = new System.Windows.Forms.SaveFileDialog();
+            this.colorPicker = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.redValue = new System.Windows.Forms.NumericUpDown();
+            this.greenValue = new System.Windows.Forms.NumericUpDown();
+            this.blueValue = new System.Windows.Forms.NumericUpDown();
+            this.RedLabel = new System.Windows.Forms.Label();
+            this.GreenLabel = new System.Windows.Forms.Label();
+            this.BlueLabel = new System.Windows.Forms.Label();
+            this.showBackColor = new System.Windows.Forms.PictureBox();
             this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.colorPicker)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.redValue)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.greenValue)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.blueValue)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.showBackColor)).BeginInit();
             this.SuspendLayout();
             // 
             // menuStrip1
@@ -45,7 +59,7 @@
             this.fileToolStripMenuItem1});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(284, 24);
+            this.menuStrip1.Size = new System.Drawing.Size(657, 24);
             this.menuStrip1.TabIndex = 0;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -62,7 +76,7 @@
             // openToolStripMenuItem
             // 
             this.openToolStripMenuItem.Name = "openToolStripMenuItem";
-            this.openToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(121, 22);
             this.openToolStripMenuItem.Text = "Open";
             this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
             // 
@@ -70,26 +84,16 @@
             // 
             this.saveAsToolStripMenuItem.Enabled = false;
             this.saveAsToolStripMenuItem.Name = "saveAsToolStripMenuItem";
-            this.saveAsToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.saveAsToolStripMenuItem.Size = new System.Drawing.Size(121, 22);
             this.saveAsToolStripMenuItem.Text = "Save as...";
             this.saveAsToolStripMenuItem.Click += new System.EventHandler(this.saveAsToolStripMenuItem_Click);
             // 
             // exitToolStripMenuItem
             // 
             this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
-            this.exitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(121, 22);
             this.exitToolStripMenuItem.Text = "Exit";
             this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
-            // 
-            // tabControl
-            // 
-            this.tabControl.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.tabControl.ItemSize = new System.Drawing.Size(0, 25);
-            this.tabControl.Location = new System.Drawing.Point(0, 24);
-            this.tabControl.Name = "tabControl";
-            this.tabControl.SelectedIndex = 0;
-            this.tabControl.Size = new System.Drawing.Size(284, 237);
-            this.tabControl.TabIndex = 2;
             // 
             // openFileDialog
             // 
@@ -99,18 +103,124 @@
             // 
             this.saveFileDialog.Filter = "JPEG (*.jpg;*.jpeg)|*.jpg;*.jpeg";
             // 
+            // colorPicker
+            // 
+            this.colorPicker.Image = global::grafikaPS2.Properties.Resources.color_wheel_260x2601;
+            this.colorPicker.Location = new System.Drawing.Point(490, 27);
+            this.colorPicker.Name = "colorPicker";
+            this.colorPicker.Size = new System.Drawing.Size(167, 163);
+            this.colorPicker.TabIndex = 1;
+            this.colorPicker.TabStop = false;
+            this.colorPicker.MouseDown += new System.Windows.Forms.MouseEventHandler(this.colorPicker_MouseDown);
+            this.colorPicker.MouseMove += new System.Windows.Forms.MouseEventHandler(this.colorPicker_MouseMove);
+            this.colorPicker.MouseUp += new System.Windows.Forms.MouseEventHandler(this.colorPicker_MouseUp);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(0, 27);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(484, 427);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseEnter += new System.EventHandler(this.pictureBox1_MouseEnter);
+            // 
+            // redValue
+            // 
+            this.redValue.Location = new System.Drawing.Point(597, 196);
+            this.redValue.Maximum = new decimal(new int[] {
+            255,
+            0,
+            0,
+            0});
+            this.redValue.Name = "redValue";
+            this.redValue.Size = new System.Drawing.Size(60, 20);
+            this.redValue.TabIndex = 2;
+            // 
+            // greenValue
+            // 
+            this.greenValue.Location = new System.Drawing.Point(597, 222);
+            this.greenValue.Maximum = new decimal(new int[] {
+            255,
+            0,
+            0,
+            0});
+            this.greenValue.Name = "greenValue";
+            this.greenValue.Size = new System.Drawing.Size(60, 20);
+            this.greenValue.TabIndex = 3;
+            // 
+            // blueValue
+            // 
+            this.blueValue.Location = new System.Drawing.Point(597, 249);
+            this.blueValue.Maximum = new decimal(new int[] {
+            255,
+            0,
+            0,
+            0});
+            this.blueValue.Name = "blueValue";
+            this.blueValue.Size = new System.Drawing.Size(60, 20);
+            this.blueValue.TabIndex = 4;
+            // 
+            // RedLabel
+            // 
+            this.RedLabel.AutoSize = true;
+            this.RedLabel.Location = new System.Drawing.Point(552, 198);
+            this.RedLabel.Name = "RedLabel";
+            this.RedLabel.Size = new System.Drawing.Size(30, 13);
+            this.RedLabel.TabIndex = 5;
+            this.RedLabel.Text = "Red:";
+            // 
+            // GreenLabel
+            // 
+            this.GreenLabel.AutoSize = true;
+            this.GreenLabel.Location = new System.Drawing.Point(552, 224);
+            this.GreenLabel.Name = "GreenLabel";
+            this.GreenLabel.Size = new System.Drawing.Size(39, 13);
+            this.GreenLabel.TabIndex = 6;
+            this.GreenLabel.Text = "Green:";
+            // 
+            // BlueLabel
+            // 
+            this.BlueLabel.AutoSize = true;
+            this.BlueLabel.Location = new System.Drawing.Point(552, 251);
+            this.BlueLabel.Name = "BlueLabel";
+            this.BlueLabel.Size = new System.Drawing.Size(31, 13);
+            this.BlueLabel.TabIndex = 7;
+            this.BlueLabel.Text = "Blue:";
+            // 
+            // showBackColor
+            // 
+            this.showBackColor.Location = new System.Drawing.Point(490, 198);
+            this.showBackColor.Name = "showBackColor";
+            this.showBackColor.Size = new System.Drawing.Size(54, 66);
+            this.showBackColor.TabIndex = 8;
+            this.showBackColor.TabStop = false;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
-            this.Controls.Add(this.tabControl);
+            this.ClientSize = new System.Drawing.Size(657, 456);
+            this.Controls.Add(this.showBackColor);
+            this.Controls.Add(this.BlueLabel);
+            this.Controls.Add(this.GreenLabel);
+            this.Controls.Add(this.RedLabel);
+            this.Controls.Add(this.blueValue);
+            this.Controls.Add(this.greenValue);
+            this.Controls.Add(this.redValue);
+            this.Controls.Add(this.colorPicker);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
             this.Text = "Form1";
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.colorPicker)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.redValue)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.greenValue)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.blueValue)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.showBackColor)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -123,9 +233,17 @@
         private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem saveAsToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
-        private System.Windows.Forms.TabControl tabControl;
         private System.Windows.Forms.OpenFileDialog openFileDialog;
         private System.Windows.Forms.SaveFileDialog saveFileDialog;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox colorPicker;
+        private System.Windows.Forms.NumericUpDown redValue;
+        private System.Windows.Forms.NumericUpDown greenValue;
+        private System.Windows.Forms.NumericUpDown blueValue;
+        private System.Windows.Forms.Label RedLabel;
+        private System.Windows.Forms.Label GreenLabel;
+        private System.Windows.Forms.Label BlueLabel;
+        private System.Windows.Forms.PictureBox showBackColor;
     }
 }
 
--- grafikaPS2/Form1.cs
+++ grafikaPS2/Form1.cs
@@ -2,11 +2,18 @@
 using System.IO;
 using System.Drawing;
 using System.Windows.Forms;
+using System.Drawing.Imaging;
+using System.Threading;
 
 namespace grafikaPS2
 {
     public partial class Form1 : Form
     {
+        private Image image;
+        bool ChooseState = false;
+        Bitmap color_bmp;
+        Color pointColor;
+
         public Form1(string path)
             : this()
         {
@@ -16,6 +23,17 @@ namespace grafikaPS2
         public Form1()
         {
             InitializeComponent();
+
+            //var bitmap = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            //for (var i = 0; i < pictureBox1.Width; i++)
+            //{
+            //    for (var j = 0; j < pictureBox1.Height; j++)
+            //    {
+            //        bitmap.SetPixel(i, j, Color.Red);
+            //    }
+            //}
+
+            //pictureBox1.Image = bitmap;
         }
 
         private void openToolStripMenuItem_Click(object sender, EventArgs e)
@@ -31,36 +49,36 @@ namespace grafikaPS2
 
         private void saveAsToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            if (tabControl.TabCount < 1) return;
+            //if (tabControl.TabCount < 1) return;
 
-            PictureTabPage tab = (PictureTabPage)tabControl.SelectedTab;
-            Image image = tab.Image;
+            //PictureTabPage tab = (PictureTabPage)tabControl.SelectedTab;
+            //Image image = tab.Image;
 
-            if (saveFileDialog.ShowDialog() == DialogResult.OK)
-            {
-                string path = saveFileDialog.FileName;
-                string extension = Path.GetExtension(saveFileDialog.FileName);
+            //if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            //{
+            //    string path = saveFileDialog.FileName;
+            //    string extension = Path.GetExtension(saveFileDialog.FileName);
 
-                Cursor = Cursors.WaitCursor;
+            //    Cursor = Cursors.WaitCursor;
 
-                try
-                {
-                    if (extension == ".pbm" || extension == ".pgm" || extension == ".ppm")
-                    {
-                        Anymap.Save(image, path);
-                    }
-                    else
-                    {
-                        image.Save(path);
-                    }
-                }
-                catch (Exception)
-                {
-                    MessageBox.Show(string.Format("Could not save file as {0}!", path), "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                }
+            //    try
+            //    {
+            //        if (extension == ".pbm" || extension == ".pgm" || extension == ".ppm")
+            //        {
+            //            Anymap.Save(image, path);
+            //        }
+            //        else
+            //        {
+            //            image.Save(path);
+            //        }
+            //    }
+            //    catch (Exception)
+            //    {
+            //        MessageBox.Show(string.Format("Could not save file as {0}!", path), "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            //    }
 
-                Cursor = Cursors.Default;
-            }
+            //    Cursor = Cursors.Default;
+            //}
         }
 
         private void exitToolStripMenuItem_Click(object sender, EventArgs e)
@@ -70,44 +88,81 @@ namespace grafikaPS2
 
         private async void LoadImage(string path)
         {
-            string filename = Path.GetFileName(path);
-            string extension = Path.GetExtension(path);
+            //string filename = Path.GetFileName(path);
+            //string extension = Path.GetExtension(path);
 
-            PictureTabPage tab = new PictureTabPage(filename);
-            tabControl.TabPages.Add(tab);
-            tabControl.SelectedTab = tab;
-            tab.Cursor = Cursors.WaitCursor;
+            //PictureBox pictureBox = new PictureBox(filename);
+            
+            //PictureTabPage tab = new PictureTabPage(filename);
+            //tabControl.TabPages.Add(tab);
+            //tabControl.SelectedTab = tab;
+            //tab.Cursor = Cursors.WaitCursor;
 
-            try
-            {
-                Image image;
-                if (extension == ".pbm" || extension == ".pgm" || extension == ".ppm")
-                {
-                    image = await Anymap.LoadAsync(path);
-                }
-                else
-                {
-                    image = Image.FromFile(path);
-                }
+            //try
+            //{
+            //    Image image;
+            //    if (extension == ".pbm" || extension == ".pgm" || extension == ".ppm")
+            //    {
+            //        image = await Anymap.LoadAsync(path);
+            //    }
+            //    else
+            //    {
+            //        image = Image.FromFile(path);
+            //    }
 
-                tab.Image = image;
-                saveAsToolStripMenuItem.Enabled = true;
-            }
-            catch (Exception ex)
-            {
-                tabControl.TabPages.Remove(tab);
-                MessageBox.Show(string.Format("Error loading {0}!", path), "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            }
+            //    tab.Image = image;
+            //    saveAsToolStripMenuItem.Enabled = true;
+            //}
+            //catch (Exception ex)
+            //{
+            //    tabControl.TabPages.Remove(tab);
+            //    MessageBox.Show(string.Format("Error loading {0}!", path), "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            //}
 
-            tab.Cursor = Cursors.Default;
+            //tab.Cursor = Cursors.Default;
         }
 
         private void tabControl_TabClosed(object sender, EventArgs e)
         {
-            if (tabControl.TabCount < 1)
-            {
-                saveAsToolStripMenuItem.Enabled = false;
+            //if (tabControl.TabCount < 1)
+            //{
+            //    saveAsToolStripMenuItem.Enabled = false;
+            //}
+        }
+
+        private void colorPicker_MouseDown(object sender, MouseEventArgs e)
+        {
+            ChooseState = true;
+            if (e.Button == System.Windows.Forms.MouseButtons.Left)
+                pictureBox1.BackColor = pointColor;
+            else if (e.Button == System.Windows.Forms.MouseButtons.Right)
+                pictureBox1.BackColor = Color.Transparent;
+        }
+
+        private void colorPicker_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (ChooseState == false)
+            {                
+                color_bmp = (Bitmap)colorPicker.Image.Clone();
+                if (e.X < color_bmp.Width && e.Y < color_bmp.Height)
+                {
+                    pointColor = color_bmp.GetPixel(e.X, e.Y);
+                    redValue.Value = pointColor.R;
+                    greenValue.Value = pointColor.G;
+                    blueValue.Value = pointColor.B;
+                    showBackColor.BackColor = pointColor;
+                } 
             }
+        }
+
+        private void pictureBox1_MouseEnter(object sender, EventArgs e)
+        {
+            pictureBox1.Focus();
+        }
+
+        private void colorPicker_MouseUp(object sender, MouseEventArgs e)
+        {
+            ChooseState = false;
         }
     }
 }
